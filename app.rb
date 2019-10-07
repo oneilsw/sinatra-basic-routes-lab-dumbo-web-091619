e7a9f89e18ab460e098a98ca0263214b9727112e
Oneil Williams <oneilswilliams@gmail.com>
2019-10-07 20:22:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end 
+  
+  get '/name' do 
+    "My name is Oneil"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Brooklyn"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is Shake it fast"
+  end 
 end
